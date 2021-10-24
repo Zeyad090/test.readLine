5a744809463445fa2b21a3cdd65f2003c3d1b4c8
zeyad <zeyad.altwalah@gmail.com>
2021-10-24 22:53:54 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner // a class (type) from the Java standard library
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val word = scanner.next()
+    val String = scanner.next()
+    val word2 = scanner.next()
+    val String2= scanner.next()
+    val B = scanner.next()
+
+    println(word)
+   println(String)
+    println(word2)
+    println(String2)
+    println(B)
+
+
+}
